25014e3a36d0e593969acbeb2c56d45bc92c4c3f
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-18 17:11:10 +0100

    zero value

--- variables_values_types/types.go
+++ variables_values_types/types.go
@@ -6,6 +6,10 @@ import (
 
 var y = 42
 var x = `backticks are strings`
+
+// DECLARE a variable to be a certain TYPE
+// and then ASSIGN a VALUE of that type to the variable
+// Cannot reassign to another type
 var z string = "shaken not stirred"
 
 func main() {
