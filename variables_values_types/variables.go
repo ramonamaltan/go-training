b8c48684faeaaf02d6c5dab6eac0a9da6be99668
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-18 16:51:06 +0100

    declaring variables

---
+++ variables_values_types/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+var y = 26
+
+// declare there is a variable with the identifier z and type int
+// assigns the zero value of type int to z
+var z int
+
+func main() {
+	// short declaration operator
+	// declare a variable and assign a value (of a certain type)
+	// cannot used outside function body
+	x := 42
+	fmt.Println(x)
+	// variables declared with var can live outside function body
+	fmt.Println(y)
+
+	foo()
+
+	fmt.Println(z)
+}
+
+func foo() {
+	fmt.Println("print again", y)
+}
