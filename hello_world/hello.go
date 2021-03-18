b3ade0ce180938206e59be72d5074ce88ba71c8b
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-18 18:04:44 +0100

    go naming convention rename file

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	x := 42
+	fmt.Println(x)
 	fmt.Println("Hello World")
 	foo()
 	fmt.Println("after the function I print this. This is the control flow.")
