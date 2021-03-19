3e0dcd5fd8cf409c46088d76ee191e7359b8b434
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-19 10:17:08 +0100

    iota

--- programming_fundamentals/constants.go
+++ programming_fundamentals/constants.go
@@ -6,6 +6,13 @@ const a = 42
 const b = 42.78
 const c = "James Bond"
 
+// ioata automatically increments by 1
+const (
+	x = iota
+	y = iota
+	z = iota
+)
+
 func main() {
 	fmt.Println(a)
 	fmt.Println(b)
@@ -13,4 +20,11 @@ func main() {
 	fmt.Printf("%T\n", a)
 	fmt.Printf("%T\n", b)
 	fmt.Printf("%T\n", c)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", z)
 }
