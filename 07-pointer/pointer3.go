c3900079449bb8c052e2408ea2c9d24d7f34d859
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-23 10:55:57 +0100

    deleted pointer3

--- 07-pointer/pointer3.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// methods set: methods attached to a type
-// a non-pointer receiver: works with pointers or non-pointers
-// a pointer receiver: only works with values that are pointers
-
-func main() {
-
-}
