ba2eb6123f296d9e5d15f69b67599564f74dc72a
Ramona Maltan <ramonamaltan@gmail.com>
2021-03-24 08:59:13 +0100

    first test

--- 01-hello_world/hello.go
+++ 01-hello_world/hello.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	x := 42
-	fmt.Println(x)
-	fmt.Println("Hello World")
-	foo()
-	fmt.Println("after the function I print this. This is the control flow.")
+	// x := 42
+	// fmt.Println(x)
+	fmt.Println(Hello())
+	// fmt.Println("after the function I print this. This is the control flow.")
 }
 
-func foo() {
-	fmt.Println("This is a function")
+func Hello() string {
+	return "Hello, world"
 }
